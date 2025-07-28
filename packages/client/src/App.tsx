04b8fbbb14dc5f11272aaafdca4f508b871a3017
Brandon J. Baker <brandon.j.baker1011@gmail.com>
2025-07-28 17:30:03 -0400

    updated user flow & installed missing packages.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,12 +1,22 @@
 import AuthPage from './pages/Auth/AuthPage';
 import './App.css';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 function App() {
   return (
-  <>
+    <Auth0Provider
+      domain={process.env.AUTH0_DOMAIN ?? ""}
+      clientId={process.env.AUTH0_CLIENT_ID ?? ""}
+      authorizationParams={
+        {
+          redirect_uri : process.env.REDIRECT_URI,
+          audience: process.env.AUTH0_AUDIENCE,
+        }
+      }
+    >
     <h1>freeDash Client</h1>
     <AuthPage />
-  </>
+    </Auth0Provider>
   );
 }
 
--- packages/client/src/api/auth.ts
+++ packages/client/src/api/auth.ts
@@ -1,3 +1,19 @@
 // Handles Auth0 interactions.
+import axios from "axios";
+import { useAuth0Auth } from "../features/auth/hooks/useAuth0";
 
-export {};
+export const createdAuthenticatedClient = () => {
+    const getToken = useAuth0Auth();
+
+    const client = axios.create({
+        baseURL: process.env.API_RUL,
+    });
+
+    client.interceptors.request.use(async (config) => {
+        const token = await getToken();
+        config.headers.Authorization = `Bearer ${token}`;
+        return config;
+    });
+
+    return client;
+};
