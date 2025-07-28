f3ee8429a6e5bb38722d57ab6e9db3122ba6d017
Brandon J. Baker <brandon.j.baker1011@gmail.com>
2025-07-28 19:10:32 -0400

    updated logic and placeholder buttons.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,20 +2,33 @@ import AuthPage from './pages/Auth/AuthPage';
 import './App.css';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+
 function App() {
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+  const redirectUri = process.env.REACT_APP_REDIRECT_URI;
+
+  if (!domain) {
+    throw new Error('REACT_APP_AUTH0_DOMAIN is not set in the .env file.');
+  }
+  if (!clientId) {
+    throw new Error('REACT_APP_AUTH0_CLIENT_ID is not set in the .env file.');
+  }
+  if (!redirectUri) {
+    throw new Error('REACT_APP_REDIRECT_URI is not set in the .env file.');
+  }
   return (
     <Auth0Provider
-      domain={process.env.AUTH0_DOMAIN ?? ""}
-      clientId={process.env.AUTH0_CLIENT_ID ?? ""}
+      domain={domain}
+      clientId={clientId}
       authorizationParams={
         {
-          redirect_uri : process.env.REDIRECT_URI,
-          audience: process.env.AUTH0_AUDIENCE,
+          redirect_uri: redirectUri,
         }
       }
     >
-    <h1>freeDash Client</h1>
-    <AuthPage />
+      <h1>freeDash Client</h1>
+      <AuthPage />
     </Auth0Provider>
   );
 }
--- packages/client/src/api/auth.ts
+++ packages/client/src/api/auth.ts
@@ -1,16 +1,16 @@
 // Handles Auth0 interactions.
 import axios from "axios";
-import { useAuth0Auth } from "../features/auth/hooks/useAuth0";
+import { useAuth0 } from "@auth0/auth0-react";
 
-export const createdAuthenticatedClient = () => {
-    const getToken = useAuth0Auth();
+export const useAuthenticatedClient = () => {
+    const { getAccessTokenSilently } = useAuth0();
 
     const client = axios.create({
         baseURL: process.env.API_RUL,
     });
 
     client.interceptors.request.use(async (config) => {
-        const token = await getToken();
+        const token = await getAccessTokenSilently();
         config.headers.Authorization = `Bearer ${token}`;
         return config;
     });
