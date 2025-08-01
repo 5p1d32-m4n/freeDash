2c75ccfec888a77c6b941b1608cf0cee165af006
Brandon J. Baker <brandon.j.baker1011@gmail.com>
2025-08-01 16:08:21 -0400

    laoding state placeholder.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 import AuthPage from './pages/Auth/AuthPage';
 import HomeDashboard from './pages/Dashboard/HomeDashboard';
 import AuthRoute from './routes/AuthRoute';
 
 const App = () => {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return (
+      <div className="page-loader" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        {/* Replace this with a more sophisticated spinner component later */}
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+
   return (
     <Routes>
       {/* Public route for the login page */}
