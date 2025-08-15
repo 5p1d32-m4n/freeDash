365aacfb52bf71b69e5fef98465f1f328a396b02
Brandon J. Baker <brandon.j.baker1011@gmail.com>
2025-08-15 14:30:26 -0400

    version deprecation change.

--- packages/shared/src/schemas/users.ts
+++ packages/shared/src/schemas/users.ts
@@ -1,11 +1,11 @@
-import {email, z} from "zod";
+import { z } from "zod";
 
 const OnboardingStatus = z.enum(["incomplete", "complete"])
 
 export const UserSchema = z.object({
     id: z.uuid(),
     auth0Id: z.string(),
-    email: z.email(),
+    email: z.string().email(),
     name: z.string().optional().nullable(),
     onboardingStatus: OnboardingStatus,
     defaultCurrency: z.string().optional(),
